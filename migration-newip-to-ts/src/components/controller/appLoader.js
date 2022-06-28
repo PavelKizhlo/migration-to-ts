bd4dd8013d566d4133f0b1a46dbac228c5ccb499
Pavel Kizhlo <pavelkizh@gmail.com>
2022-06-28 17:53:29 +0300

    feat: add API key to appLoader.js

--- migration-newip-to-ts/src/components/controller/appLoader.js
+++ migration-newip-to-ts/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '16762e38ae944c428856d81441dcd040', // получите свой ключ https://newsapi.org/
         });
     }
 }
