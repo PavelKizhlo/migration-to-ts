86187dea7bd7b3d00e0008797a6877594ab404e6
Pavel Kizhlo <pavelkizh@gmail.com>
2022-07-03 21:08:05 +0300

    feat: add rss-logo and gh-link to footer, add placeholders

--- migration-newip-to-ts/src/components/app/app.ts
+++ migration-newip-to-ts/src/components/app/app.ts
@@ -19,7 +19,7 @@ class App {
     }
 
     private sortSources(): void {
-        setTimeout(this.showSorted.bind(this), 200);
+        setTimeout(this.showSorted.bind(this), 350);
         const alphabet = document.querySelector('.alphabet') as HTMLElement;
 
         alphabet.addEventListener('click', (e) => {
@@ -36,6 +36,9 @@ class App {
     }
 
     private showSorted(letter = 'A') {
+        let sourceCounter = 0;
+        const sourcePlaceholder = document.querySelector('.sources-placeholder') as HTMLElement;
+
         document.querySelectorAll('.source__item').forEach((item) => {
             const sourceId = (item as HTMLElement).dataset.sourceId as string;
             const firstLetter = sourceId[0].toUpperCase();
@@ -44,8 +47,15 @@ class App {
                 (item as HTMLElement).style.display = 'none';
             } else {
                 (item as HTMLElement).style.display = 'block';
+                sourceCounter += 1;
             }
         });
+
+        if (sourceCounter) {
+            sourcePlaceholder.style.display = 'none';
+        } else {
+            sourcePlaceholder.style.display = 'block';
+        }
     }
 }
 
